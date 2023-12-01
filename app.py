b0ca8604fd2e2b28560e348752ee4956d1402e33
Jaroslav Mudrak <jaroslav.mudrak@gmail.com>
2023-12-01 14:58:23 +0000

    Adding web elements

--- app.py
+++ app.py
@@ -21,10 +21,10 @@ else:
 data_df = pd.DataFrame(data)
 data_df = data_df.reset_index()
 
-# st.line_chart(data_df)
+st.write(data_df)
 
-#create chart:
-fig = sns.lineplot(x=data_df.Date, y=data_df.Close)
+# #create chart:
+# sns.lineplot(x=data_df.Date, y=data_df.Close)
 
-#display chart using st.pyplot
-st.pyplot(plt.gcf())
+# #display chart using st.pyplot
+# st.pyplot(plt.gcf())
