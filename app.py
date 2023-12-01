bb1adb9ffe7afd9e571a14b14c7c64777fa591f7
Jaroslav Mudrak <jaroslav.mudrak@gmail.com>
2023-12-01 15:16:22 +0000

    Adding web elements

--- app.py
+++ app.py
@@ -12,18 +12,22 @@ url = 'https://stocks-4dlywuyz2q-ew.a.run.app/predict?n=100'
 
 response = requests.get(url, verify=False)
 
+
+#Get data from API
 if response.status_code == 200:
     data = response.json()  # Assuming the API returns JSON data
-    st.write('Fetching succesfull')
 else:
     st.error(f"Error: Unable to fetch data from API. Status code: {response.status_code}")
 
+
+#Format the data into useful dataframe:
 data_df = pd.DataFrame(data)
 data_df = data_df.reset_index()
 data_df = data_df.rename(columns={'index':'Date'})
 data_df['Date']= pd.to_datetime(data_df['Date'])
 
-st.write(data_df)
+#Show table
+# st.write(data_df)
 
 #create chart:
 sns.lineplot(x=data_df['Date'], y=data_df['Close'])
