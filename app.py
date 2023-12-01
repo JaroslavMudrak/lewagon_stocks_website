521b9fac2930b7883dd8c18ac82c5959bf468b50
Jaroslav Mudrak <jaroslav.mudrak@gmail.com>
2023-12-01 17:00:50 +0000

    Adding web elements

--- app.py
+++ app.py
@@ -8,9 +8,13 @@ import matplotlib.pyplot as plt
 ## How amazing is this? Very much, indeed.
 '''
 
-url = 'https://stocks-4dlywuyz2q-ew.a.run.app/predict?n=100'
 
-response = requests.get(url, verify=False)
+##########  Get predictions ############################
+
+
+url_predict = 'https://stocks-4dlywuyz2q-ew.a.run.app/predict?n=697'
+
+response = requests.get(url_predict, verify=False)
 
 
 #Get data from API
@@ -21,16 +25,57 @@ else:
 
 
 #Format the data into useful dataframe:
-data_df = pd.DataFrame(data)
-data_df = data_df.reset_index()
-data_df = data_df.rename(columns={'index':'Date'})
-data_df['Date']= pd.to_datetime(data_df['Date'])
+df_pred = pd.DataFrame(data)
+df_pred = df_pred.rename(columns={'Close':'Prediction'})
+# df_pred = df_pred.reset_index()
+# df_pred = df_pred.rename(columns={'index':'Date'})
+# df_pred['Date']= pd.to_datetime(df_pred['Date'])
+
+
+#########################################################
+
+
+################ Get Actuals ############################
+
+url_actuals = 'https://stocks-4dlywuyz2q-ew.a.run.app/actuals'
+response_actuals = requests.get(url_actuals, verify=False)
+
+#Get data from API
+if response.status_code == 200:
+    data_actuals = response_actuals.json()  # Assuming the API returns JSON data
+else:
+    st.error(f"Error: Unable to fetch data from API. Status code: {response.status_code}")
+
+#########################################################
+
+#Format the data into useful dataframe:
+df_actuals = pd.DataFrame(data_actuals)
+df_actuals = df_actuals.rename(columns={'Close':'Actuals'})
+
+
+# df_actuals['Date']= pd.to_datetime(df_actuals['Date'])
+
+
+################ Merge data together ####################
+
+final_df = pd.concat([df_actuals, df_pred], axis=1)
+# final_df = final_df.reset_index()
+# final_df = final_df.rename(columns={'index':'Date'})
+
+
+
+#########################################################
+
+# VISUALS
 
 #Show table
-# st.write(data_df)
+# st.write(df_actuals)
+# st.write(final_df)
 
 #create chart:
-sns.lineplot(x=data_df['Date'], y=data_df['Close'])
+sns.lineplot(data=final_df)
 
-#display chart using st.pyplot
+
+
+# #display chart using st.pyplot
 st.pyplot(plt.gcf())
