521b6d370dee768cf4021c4c269bfecc4974cb0f
Jaroslav Mudrak <jaroslav.mudrak@gmail.com>
2023-12-01 11:45:34 +0000

    API test

--- app.py
+++ app.py
@@ -8,4 +8,11 @@ import requests
 url = 'https://stocks-4dlywuyz2q-ew.a.run.app/predict?n=2'
 
 response = requests.get(url)
-response
+
+if response.status_code == 200:
+    data = response.json()  # Assuming the API returns JSON data
+    st.write('Fetching succesfull')
+else:
+    st.error(f"Error: Unable to fetch data from API. Status code: {response.status_code}")
+
+data
