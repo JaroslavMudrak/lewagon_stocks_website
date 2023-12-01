305c9495bed587d6db23023e8f7d9a470e7d70ac
Jaroslav Mudrak <jaroslav.mudrak@gmail.com>
2023-12-01 11:43:00 +0000

    API test

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import requests
 
 '''
-# Stock prediction - How amazing is this? Very much!
+# How amazing is this? Very much, indeed.
 '''
+
+url = 'https://stocks-4dlywuyz2q-ew.a.run.app/predict?n=2'
+
+response = requests.get(url)
+response
